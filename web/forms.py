509969f904f79c3bf1a508710c19968b5694178f
David Valencia <u20192182074@usco.edu.co>
2022-10-02 20:59:50 -0500

    Primer commit a railway

---
+++ web/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from django.contrib.auth.models import User
+
+from .models import Car
+
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'username', 'password']
+
+
+
+class CompareForm(forms.Form):
+
+    car1 = forms.ModelChoiceField(
+        Car.objects.all(),
+        required=True,
+        widget=forms.Select(
+            attrs={'class': 'selectpicker', 'data-live-search': "true"}
+        )
+    )
+    car2 = forms.ModelChoiceField(
+        Car.objects.all(),
+        required=True,
+        widget=forms.Select(
+            attrs={'class': 'selectpicker', 'data-live-search': "true"}
+        )
+    )
